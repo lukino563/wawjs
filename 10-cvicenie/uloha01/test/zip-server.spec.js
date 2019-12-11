5c4b86984e6f7d74d37f77e72a92b8d8d76d4b60
lukino563 <lukino563@gmail.com>
2019-12-11 22:58:44 +0100

    new test for http

--- 10-cvicenie/uloha01/test/zip-server.spec.js
+++ 10-cvicenie/uloha01/test/zip-server.spec.js
@@ -42,4 +42,26 @@ describe("Zipper Tests", () => {
             fs.unlinkSync(`${__dirname}/${path.parse(file).base}.gz`);
         });
     });
+
+    it("Client disconnect", () => {
+
+        srv.close();
+        srv = server(dir).listen(port);
+
+        let clt = client(port, file).on('finish', ()=>{
+            srv.close();
+        });
+        let count = 0;
+
+        clt.on('data', ()=>{
+            if (count++ < 3) {
+                clt.destroy();
+            }
+        })
+
+        srv.on('close', () => {
+            assert(!fs.existsSync(`${__dirname}/${path.parse(file).base}.gz`));
+            assert(!fs.existsSync(`${dir}/testfile.bin`));
+        });
+    });
 });
